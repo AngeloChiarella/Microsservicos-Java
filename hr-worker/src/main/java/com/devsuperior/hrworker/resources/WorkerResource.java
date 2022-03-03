7df7b8d8c31c85091feaa569c20518f9cc7ec12a
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-03-03 16:39:26 -0300

    Private Git repository

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -51,11 +51,10 @@ public class WorkerResource {
 	try {
 	    Thread.sleep(3000L);
 	} catch (InterruptedException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 
-	logger.info("PORT = " + env.getProperty("local.server.port"));// Mostrar o número da porta que está rodando
+	logger.info("PORT = " + env.getProperty("local.server.port"));
 
 	Worker obj = repository.findById(id).get();
 	return ResponseEntity.ok(obj);
